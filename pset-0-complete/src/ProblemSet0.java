086257fc3de0d0e26856d8452b4069914033c127
demitech1024 <demitech1024@gmail.com>
2019-09-10 00:23:31 -0400

    Completion and compilation of Problem Set 0.

--- pset-0-complete/src/ProblemSet0.java
+++ pset-0-complete/src/ProblemSet0.java
@@ -117,8 +117,8 @@ public class ProblemSet0 {
          * Print a plus sign to the console.
          */
 
-        System.out.println("\t+---+\n\t|\t|");
-        System.out.println("+---+\t+---+\n|\t\t\t|\n+---+\t+---+");
-        System.out.println("\t|\t|\n\t+---+");
+        System.out.println("\t\b\b\b\b+---+\n\t\b\b\b\b|\t|");
+        System.out.println("+---+\t+---+\n|\t\t\b\b\b\b|\n+---+\t+---+");
+        System.out.println("\t\b\b\b\b|\t|\n\t\b\b\b\b+---+\n");
     }
 }
